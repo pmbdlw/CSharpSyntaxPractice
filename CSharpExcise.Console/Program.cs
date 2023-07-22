f68b2ee0783eacae2c2c88cc3857bf3e60c27b8a
liuyide <yide.liu@live.com>
2023-07-22 18:51:15 +1200

    add test files and appsettings.json file

--- CSharpExcise.Console/Program.cs
+++ CSharpExcise.Console/Program.cs
@@ -1,4 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using CSharpExcise.Console.TestLib;
+
 Console.WriteLine("Hello, World!");
 Console.ReadLine();
+var w1 = new WithKeywordTest("name1", "age1");
+var w2 = w1 with { Name = "name2" };
+Console.WriteLine(w1.ReturnDetail());
+Console.WriteLine(w2.ReturnDetail());
