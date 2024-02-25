7fb3462b91681a9e3c15f5c8bb80b3f51a14b30c
Andrew Liu <yide.liu@live.com>
2024-02-26 10:36:25 +1300

    modify .project for MinimalAPIBySelf-section compile

--- MinimalAPIBySelf/Db/Models/SysUserEntityConfiguration.cs
+++ MinimalAPIBySelf/Db/Models/SysUserEntityConfiguration.cs
@@ -1,6 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace MinimalAPIBySelf.Db.Models;
+
+/// <summary>
+/// SysUser Entity Config
+/// </summary>
 public class SysUserEntityConfiguration : IEntityTypeConfiguration<SysUserEntity>
 {
+    public void Configure(EntityTypeBuilder<SysUserEntity> builder) {
+        
+    }
 }
